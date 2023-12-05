9b45e84d45bc7507a36a7851067fcdc035d575f8
cksmith21 <cksmith21@gmail.com>
2023-12-04 19:56:01 -0500

    the worst code imaginable

--- DayOne/parse_numbers.py
+++ DayOne/parse_numbers.py
@@ -1,125 +1,51 @@
+# part one
+
 sum = 0 
 valid_strings = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# with open("input.txt", "r") as file: 
-
-#     for line in file: 
-#         string_number_list = []
-#         for c in line: 
-#             if c.isnumeric(): 
-#                 string_number_list.append(c) 
-
-#         curr_line = string_number_list[0] + string_number_list[-1]
-#         print(curr_line)
-#         sum += int(curr_line)
-
-# print(sum)
-            
-
 with open("input.txt", "r") as file: 
 
     for line in file: 
+        string_number_list = []
+        for c in line: 
+            if c.isnumeric(): 
+                string_number_list.append(c) 
 
-        curr_line = ''
+        curr_line = string_number_list[0] + string_number_list[-1]
+        print(curr_line)
+        sum += int(curr_line)
 
-        # create lists needed
+print(sum)
 
-        items = [] 
-        first_word_occurances = [line.find(x) for x in valid_strings]
-        last_word_occurances = [line.rfind(x) for x in valid_strings]
+# part two
 
-        print(last_word_occurances)
+number_dict = {"zero": "z0o", "one": "o1e", "two": "t2o", "three": "t3e", 
+               "four": "f4r", "five": "f5e", "six": "s6x", "seven": "s7n",
+               "eight": "e8t", "nine": "n9e"}
+sum = 0          
 
-        # find the first occurance of a word number
-        min_word_occurance = min(first_word_occurances)
-        min_word_occurance_index = first_word_occurances.index(min_word_occurance)
+with open("input.txt", "r") as file: 
 
-        # find the last occurance of a word number
-        max_word_occurance = max(last_word_occurances)
-        max_word_occurance_index = last_word_occurances.index(max_word_occurance)
+    for line in file:
 
-        # min_number_index = 0
-        # max_number_index = len(line)-1
-        # max_number = 0
+        curr_line = '' 
 
-        # reversed_line = line[::-1]
+        for key in number_dict:
 
-        # for c in line:
-        #     if c.isnumeric():
-        #         break
-        #     min_number_index += 1
+            if key in line:
 
-        # for c in reversed_line:
-        #     if c.isnumeric():
-        #         break
-        #     max_number_index -= 1
-        #     max_number += 1
+                line = line.replace(key, number_dict[key])
 
-        # if min_word_occurance < min_number_index:
-        #     curr_line += str(numbers[min_word_occurance_index])
-        # else: 
-        #     curr_line += line[min_number_index]
+        print(line)
 
-        # if max_word_occurance > max_number_index:
-        #     curr_line += str(numbers[max_word_occurance_index])
-        # else: 
-        #     curr_line += line[max_number]
+        string_number_list = []
+        for c in line: 
+            if c.isnumeric(): 
+                string_number_list.append(c) 
 
-        # print(curr_line)
-
-       
-        
-        item_index = 0 
-        is_word = False
-        for c in line:
-            if c.isnumeric():
-                break
-            item_index += 1
-
-        for index, item in enumerate(first_word_occurances): 
-            if item < item_index and item != -1:
-                is_word = True
-                curr_line += str(numbers[index])
-
-        if not is_word: 
-            curr_line += line[item_index]
-
-        
-
-        # get index of max word occurance
-
-        max_word_occurance = max(last_word_occurances)
-        max_word_occurance_index = last_word_occurances.index(max_word_occurance)
-
-        item_index = len(line)-1
-        number = 0
-        is_word = False
-        reversed_line = line[::-1]
-
-        for c in reversed_line: 
-            if c.isnumeric():
-                break
-            item_index -= 1
-            number += 1
-
-        print(max_word_occurance)
-        if max_word_occurance > item_index:
-            is_word = True
-            curr_line += str(numbers[max_word_occurance_index])
-            sum += int(curr_line)
-
-        else: 
-
-            for c in reversed_line: 
-                if c.isnumeric():
-                    curr_line += c
-                    break
-            sum += int(curr_line)
-
-        if int(curr_line) > 99:
-            print(line)
-            print(curr_line)
-        
+        curr_line = string_number_list[0] + string_number_list[-1]
+        print(curr_line)
+        sum += int(curr_line)
 
 print(sum)
